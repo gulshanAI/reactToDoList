687a1fc608e95557e3a10d23f9f60ae2bd0d408f
Gulshan <gulshan@localhost.localdomain>
2020-06-19 21:21:40 +0530

    React Simple TODO list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      newItem : "",
+      list : []
+    }
+  }
+  changeNew = (e) =>{
+    this.setState({ newItem : e.target.value})
+  }
+  addList = (e) =>{
+    const newList = {
+      id : 1 + Math.random(),
+      value : this.state.newItem
+    };
+    const list = [...this.state.list]
+    list.push(newList)
+    this.setState({ list : list })
+    this.setState({ newItem : ""})
+  }
+  delete = (id) => {
+    const list = [...this.state.list]
+    const updateList = list.filter(item=> item.id !== id)
+    this.setState({ list : updateList})
+  }
+  render(){
+    return(
+      <div>
+        <input type="text" onChange={this.changeNew} value={this.state.newItem} /> &nbsp;
+        <button onClick={this.addList}>Add</button>
+        <br/>
+        <br/>
+        <ul>
+            {this.state.list.map(item=> <li key={item.id}>{item.value} &nbsp;&nbsp;&nbsp;<button onClick={ ()=> this.delete(item.id)}>Delete</button></li>)}
+        </ul>
+      </div>
+    )
+  }
 }
-
 export default App;
